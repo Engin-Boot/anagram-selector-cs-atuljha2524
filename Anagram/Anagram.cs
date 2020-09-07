56905c5119ad8bcb9d9b7f85f29b139d3f1f004f
atuljha2524 <69622477+atuljha2524@users.noreply.github.com>
2020-09-07 08:50:55 +0530

    Update Anagram.cs

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -7,10 +7,20 @@ namespace Anagram
     {
         public bool WordPairIsAnagram(string word1, string word2) {
             //Insert the correct implementation here
-            return false;
+            char [] charArray1 = word1.ToCharArray();
+            char [] charArray2 = word2.ToCharArray();
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+            return charArray1.Equals(charArray2);
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
             //Insert the correct implementation here
+            for (int i = 0; i < candidates.Count; i++) {
+                if (!WordPairIsAnagram(word, candidates[i])) {
+                    candidates.RemoveAt(i);
+                    i--;
+                }
+            }
             return candidates;
         }
     }
